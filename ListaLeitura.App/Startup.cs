cd9b9f116486098711a96365432b572753881131
Elias Alves do Nascimento <elias.nascimento@ltm.digital>
2022-03-12 20:12:45 -0300

    Criação de rotas, cadastro de novo livro e detalhes do livro;

--- ListaLeitura.App/Startup.cs
+++ ListaLeitura.App/Startup.cs
@@ -1,21 +1,100 @@
-﻿using ListaLeitura.App.Repositorio;
+﻿using ListaLeitura.App.Negocio;
+using ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ListaLeitura.App
 {
     public class Startup
     {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            var builder = new RouteBuilder(app);
+            builder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            builder.MapRoute("Livros/Lendo", LivrosLendo);
+            builder.MapRoute("Livros/Lidos", LivrosLidos);            
+            builder.MapRoute("Livros/Detalhes/{id:int}", ExibeDetalhes);
+            builder.MapRoute("Cadastro/NovoLivro", ExibeFormulario);
+            builder.MapRoute("Cadastro/Incluir", ProcessaFormulario);
+            var rotas = builder.Build();
+
+            app.UseRouter(rotas);
         }
 
+        //Cadastro de um novo livro
+        private Task ProcessaFormulario(HttpContext context)
+        {
+            var livro = new Livro()
+            {
+                Titulo = context.Request.Query["titulo"].First(),
+                Autor = context.Request.Query["autor"].First(),
+            };
+
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return context.Response.WriteAsync("Livro adicionado com sucesso!");
+        }
+
+        //Exibe formulário para cadastro do livro
+        private Task ExibeFormulario(HttpContext context)
+        {
+            var html = @"<html>
+                            <form action='/Cadastro/Incluir'>
+                                <label>Titulo</label>
+                                <input name='titulo'/>
+                                <br/>
+                                <label>Autor</label>
+                                <input name='autor'/>
+                                <br/>
+                                <button>Gravar</button>
+                            </form>
+                        </html>";
+
+            return context.Response.WriteAsync(html);
+        }
+
+        //Exibe detalhes do livro
+        private Task ExibeDetalhes(HttpContext context)
+        {
+            int id = Convert.ToInt32(context.GetRouteValue("id"));
+            var repo = new LivroRepositorioCSV();
+
+            var livro = repo.Todos.First(l => l.Id == id);
+
+            return context.Response.WriteAsync(livro.Detalhes());
+        }
+        
+
+        //Livros para ler
         public Task LivrosParaLer(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
             return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        //Livros lendo
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        //Livros lidos
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
         }
     }
 }

7c0f7a28e30e23ef779c07f5f3fdcf28833e1a68
Elias Alves do Nascimento <elias.nascimento@ltm.digital>
2022-03-12 17:34:48 -0300

    Add project files.

---
+++ ListaLeitura.App/Program.cs
@@ -0,0 +1,28 @@
+﻿using ListaLeitura.App.Negocio;
+using ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace ListaLeitura.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _repo = new LivroRepositorioCSV();
+
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+            
+        }
+
+        static void ImprimeLista(ListaDeLeitura lista)
+        {
+            Console.WriteLine(lista);
+        }
+    }
+}
---
+++ ListaLeitura.App/Repositorio/ILivroRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using ListaLeitura.App.Negocio;
+using System.Collections.Generic;
+
+namespace ListaLeitura.App.Repositorio
+{
+    interface ILivroRepositorio
+    {
+        ListaDeLeitura ParaLer { get; }
+        ListaDeLeitura Lendo { get; }
+        ListaDeLeitura Lidos { get; }
+        IEnumerable<Livro> Todos { get; }
+        void Incluir(Livro livro);
+    }
+}
